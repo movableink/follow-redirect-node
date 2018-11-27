870505dd41bc2227f44ca37eb61c778bed911374
Roger Rodriguez <rrodriguez@movableink.com>
2018-11-27 11:29:26 -0500

    v0.1.0

--- utils/log.js
+++ utils/log.js
@@ -1,6 +1,6 @@
 module.exports = function log(msg, header = null){
   // silence is golden in prod
-  if(process.env.NODE_ENV = 'production'){
+  if(process.env.NODE_ENV === 'production'){
     return;
   }
   header ? console.log(`--- ${header} ----`) : console.log(" ");
